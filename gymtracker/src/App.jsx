5d3d3d5e4b48deb922ef83776eb2aa7641aa032f
BENCHEIKH ABDELKARIM p2414115 <bencheikh.abd.karim@gmail.com>
2025-04-26 18:50:08 +0200

    feat(frontend): add sidebar

--- gymtracker/src/App.jsx
+++ gymtracker/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import './assets/tailwind.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <p className='text-red-400'>test tailwind css</p>
+      <Sidebar />
     </div>
   )
 }
---
+++ gymtracker/src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import HugeiconsDashboardBrowsing from "./icones/dashboard";
+import HugeiconsChart01 from './icones/chart'
+import HugeiconsNoteEdit from './icones/note'
+import HugeiconsCalendar01 from "./icones/calendar";
+
+function Sidebar() {
+    return (
+        <div>
+            <div className="grid grid-cols-1 gap-5">
+                <div className="flex place-items-center rounded-l-xl pt-2 pb-2 pl-3 pr-20 hover:bg-blue-200 hover:border-r-5 hover:text-blue-700">
+                    <HugeiconsDashboardBrowsing className="mr-3" />
+                    Dashboard
+                </div>
+                <div className="flex place-items-center rounded-l-xl pt-2 pb-2 pl-3 pr-20 hover:bg-blue-200 hover:border-r-5 hover:text-blue-700">
+                    <HugeiconsChart01 className="mr-3" />
+                    Statistics
+                </div>
+                <div className="flex place-items-center rounded-l-xl pt-2 pb-2 pl-3 pr-20 hover:bg-blue-200 hover:border-r-5 hover:text-blue-700">
+                    <HugeiconsNoteEdit className="mr-3" />
+                    Workout plans
+                </div>
+                <div className="flex place-items-center rounded-l-xl pt-2 pb-2 pl-3 pr-20 hover:bg-blue-200 hover:border-r-5 hover:text-blue-700">
+                    <HugeiconsCalendar01 className="mr-3" />
+                    Schedule
+                </div>
+                <div className="divider divider-horizontal">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar
